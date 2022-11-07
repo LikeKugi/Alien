6c5d192d5a7af82a894b4b0c46c5957f481854f5
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-11-07 11:25:28 +0300

    inited

---
+++ invasion.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+from settings import Settings
+
+
+class Invasion:
+    '''resourse management'''
+
+    def __init__(self):
+        pygame.init()
+
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption('Invasion')
+
+        #  background color
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        '''main loop start'''
+        while True:
+            #  keyboard
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.settings.bg_color)
+
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    invasion = Invasion()
+    invasion.run_game()
