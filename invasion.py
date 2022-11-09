a17cdebb945ef0876dff19a787993af658522263
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-11-09 13:14:50 +0300

    many rows

--- invasion.py
+++ invasion.py
@@ -129,24 +129,33 @@ class Invasion:
         alien = Alien(self)
 
         # calculating space between alienships and count of alienships in a row
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        #  creating a row
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        #  calculating count of rows on the screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number: int) -> None:
-        '''
+        #  creating a fleet
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number: int, row_number: int) -> None:
+        """
         create an alien and put it in a row
         :param alien_number: int (number of alien in a row)
+        :param row_number: int (number of current row)
         :return: None
-        '''
+        """
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self) -> None:
